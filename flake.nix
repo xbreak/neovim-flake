da1f7dd5eb88bb071eb8801d6dec20cd91499097
xbreak <xbreak@users.noreply.github.com>
2022-08-19 10:37:18 +0200

    Add extra plugins to flake inputs

--- flake.nix
+++ flake.nix
@@ -5,25 +5,70 @@
     # Nixpkgs
     nixpkgs.url = "nixpkgs/nixos-22.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-  };
 
+    # Neovim Plugins
+    nvim-tree-lua = {
+      url = "github:kyazdani42/nvim-tree.lua";
+      flake = false;
+    };
+    fzf-lua = {
+      url = "github:ibhagwan/fzf-lua";
+      flake = false;
+    };
+    robotframework-vim = {
+      url = "github:rasjani/robotframework-vim";
+      flake = false;
+    };
+    vim-fswitch = {
+      url = "github:derekwyatt/vim-fswitch";
+      flake = false;
+    };
+    vim-rst = {
+      url = "github:habamax/vim-rst";
+      flake = false;
+    };
+  };
   outputs = {
     self,
     nixpkgs,
     nixpkgs-unstable,
-  }: let
+    ...
+  } @ inputs: let
+    plugins = [
+      "nvim-tree-lua"
+      "fzf-lua"
+      "robotframework-vim"
+      "vim-fswitch"
+      "vim-rst"
+    ];
     system = "x86_64-linux";
-    pkgs = import nixpkgs-unstable {inherit system;};
+    pkgs = import nixpkgs-unstable {
+      inherit system;
+      overlays = [pluginOverlay];
+    };
     nvims = import ./neovim {inherit pkgs;};
+
+    pluginOverlay = top: last: let
+      buildPlug = name:
+        top.vimUtils.buildVimPluginFrom2Nix {
+          pname = name;
+          version = "master";
+          src = builtins.getAttr name inputs;
+        };
+    in {
+      neovimPlugins = builtins.listToAttrs (map (name: {
+          inherit name;
+          value = buildPlug name;
+        })
+        plugins);
+    };
   in {
     # Enables `nix fmt <file>`
     formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
 
     # Neovim packages
     packages.${system} = {
-      inherit (nvims) neovim neovim-qt;
-      neovim-lsp = nvims.neovim-lsp-alias;
-      neovim-qt-lsp = nvims.neovim-qt-lsp-alias;
+      inherit (nvims) neovim neovim-lsp neovim-qt neovim-qt-lsp;
       default = nvims.neovim;
     };
 
--- neovim/default.nix
+++ neovim/default.nix
@@ -1,72 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
+{pkgs ? import <nixpkgs> {}}: let
   core-init-vim = substitutePackages ./core/init.vim {
-            inherit (pkgs)
-              bat
-              black
-              ripgrep;
-          };
+    inherit
+      (pkgs)
+      bat
+      black
+      ripgrep
+      ;
+  };
   lib = pkgs.lib;
   substitutePackages = src: substitutions:
-    pkgs.substituteAll ({ inherit src; } // lib.mapAttrs'
-      (k: lib.nameValuePair (builtins.replaceStrings [ "-" ] [ "_" ] k))
+    pkgs.substituteAll ({inherit src;}
+      // lib.mapAttrs'
+      (k: lib.nameValuePair (builtins.replaceStrings ["-"] ["_"] k))
       substitutions);
-
-  nvim-tree-lua-master = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "nvim-tree-lua";
-    version = "2022-05-09";
-    src = pkgs.fetchFromGitHub {
-      owner = "kyazdani42";
-      repo = "nvim-tree.lua";
-      rev = "47732b6dbfda4a343f6062f8254080e75e00c8b3";
-      sha256 = "1yd1ycb3d09z47cngvpaxh4b78n747lhgxrl92xrcyd8b88hd9a7";
-    };
-    meta.homepage = "https://github.com/kyazdani42/nvim-tree.lua/";
-  };
-
-  vim-rst = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "vim-rst";
-    version = "2022-01-07";
-    src = pkgs.fetchFromGitHub {
-      owner = "habamax";
-      repo = "vim-rst";
-      rev = "fb9a37bf9f873e2c90fca9e0fd1ebf7e0782fb73";
-      sha256 = "0zbllwm044bxjx6is7cyjzrrihzhj26lppa72hx15p2v52kh6idc";
-    };
-  };
-
-  vim-fswitch = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "vim-fswitch";
-    version = "2022-01-09";
-    src = pkgs.fetchFromGitHub {
-      owner = "derekwyatt";
-      repo = "vim-fswitch";
-      rev = "94acdd8bc92458d3bf7e6557df8d93b533564491";
-      sha256 = "13nk3qwbijzx8pwsm5303dm3dd7xhr2jhchkxmfs10i962v6khrc";
-    };
-  };
-
-  robotframework-vim = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "robotframework-vim";
-    version = "2019-05-08";
-    src = pkgs.fetchFromGitHub {
-      owner = "rasjani";
-      repo = "robotframework-vim";
-      rev = "bc2156daf6e35dfa5b11001ab2acf0df286b1e6f";
-      sha256 = "0hfcw3ampkx2hcxpq580ypi6mmi0aydrjgwcgbjxxw1sypmwxvl4";
-    };
-  };
-  
-  fzf-lua = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "fzf-lua";
-    version = "2022-05-18";
-    src = pkgs.fetchFromGitHub {
-      owner = "ibhagwan";
-      repo = "fzf-lua";
-      rev = "eb3d2d121f480471a9571b6d9138b4c49a5ed3e0";
-      sha256 = "1r4kb93l1m204p0jng20aa974vqvkcqkfkkasdn0h2yp8wl8jsj3";
-    };
-  };
 
   # My own config
   dotplug = pkgs.vimUtils.buildVimPluginFrom2Nix {
@@ -75,13 +21,14 @@ let
     src = ./dotplug;
   };
 
-  corePlugins = with pkgs.vimPlugins; [
+  corePlugins = with pkgs.vimPlugins;
+  with pkgs.neovimPlugins; [
     # My config as plugin
     dotplug
 
     # Navigation
     fzf-vim
-    nvim-tree-lua-master
+    nvim-tree-lua
     lualine-nvim
     hop-nvim
     vim-fswitch
@@ -118,7 +65,7 @@ let
       packages.myVimPackage = with pkgs.vimPlugins; {
         start = corePlugins;
         # opt that dependency will always be added to start to avoid confusion.
-        opt = [ ];
+        opt = [];
       };
     };
   };
@@ -128,7 +75,7 @@ let
   };
 
   neovim-qt-lsp = pkgs.neovim-qt.override {
-    neovim=neovim-lsp;
+    neovim = neovim-lsp;
   };
 
   neovim-lsp = pkgs.neovim.override {
@@ -138,80 +85,83 @@ let
         luafile ${./core/init.lua}
         luafile ${
           substitutePackages ./lsp/init.lua {
-            inherit (pkgs)
+            inherit
+              (pkgs)
               shellcheck
-              yaml-language-server;
-            inherit (pkgs.llvmPackages_latest)
+              yaml-language-server
+              ;
+            inherit
+              (pkgs.llvmPackages_latest)
               clang
-              clang-unwrapped;
-            python-lsp-server = (pkgs.python3.override {
-              packageOverrides = _: super: {
-                python-lsp-server = super.python-lsp-server.override {
-                  withAutopep8 = false;
-                  withFlake8 = false;
-                  withMccabe = false;
-                  withPyflakes = false;
-                  withPylint = true;
-                  withYapf = false;
+              clang-unwrapped
+              ;
+            python-lsp-server =
+              (pkgs.python3.override {
+                packageOverrides = _: super: {
+                  python-lsp-server = super.python-lsp-server.override {
+                    withAutopep8 = false;
+                    withFlake8 = false;
+                    withMccabe = false;
+                    withPyflakes = false;
+                    withPylint = true;
+                    withYapf = false;
+                  };
                 };
-              };
-            }).withPackages
-              (ps: with ps; [ pyls-isort python-lsp-black python-lsp-server ]);
+              })
+              .withPackages
+              (ps: with ps; [pyls-isort python-lsp-black python-lsp-server]);
           }
         }
       '';
       packages.myVimPackage = with pkgs.vimPlugins; {
-        start = corePlugins ++ [
-          nvim-lspconfig
-          # tree-sitter
-          (nvim-treesitter.withPlugins (p: [
-            p.tree-sitter-c
-            p.tree-sitter-cpp
-            p.tree-sitter-python
-            p.tree-sitter-json
-            p.tree-sitter-yaml
-            p.tree-sitter-bash
-            p.tree-sitter-nix
-            p.tree-sitter-latex
-            p.tree-sitter-markdown
-            p.tree-sitter-rst
-          ]))
-          playground
+        start =
+          corePlugins
+          ++ [
+            nvim-lspconfig
+            # tree-sitter
+            (nvim-treesitter.withPlugins (p: [
+              p.tree-sitter-c
+              p.tree-sitter-cpp
+              p.tree-sitter-python
+              p.tree-sitter-json
+              p.tree-sitter-yaml
+              p.tree-sitter-bash
+              p.tree-sitter-nix
+              p.tree-sitter-latex
+              p.tree-sitter-markdown
+              p.tree-sitter-rst
+            ]))
+            playground
 
-          trouble-nvim
-          lspkind-nvim
-          luasnip
-          lsp_signature-nvim
+            trouble-nvim
+            lspkind-nvim
+            luasnip
+            lsp_signature-nvim
 
-          # nvim-cmp and completion sources
-          nvim-cmp
-          cmp-spell
-          cmp-buffer
-          cmp-nvim-lsp
-          cmp-nvim-lsp-document-symbol
-          cmp-nvim-lua
-          cmp-path
-          cmp_luasnip
-        ];
+            # nvim-cmp and completion sources
+            nvim-cmp
+            cmp-spell
+            cmp-buffer
+            cmp-nvim-lsp
+            cmp-nvim-lsp-document-symbol
+            cmp-nvim-lua
+            cmp-path
+            cmp_luasnip
+          ];
       };
     };
   };
-
+in {
+  inherit
+    neovim
+    neovim-qt
+    ;
   # Alias program for lsp
-  neovim-lsp-alias = pkgs.writeShellScriptBin "nvim-lsp" ''
+  neovim-lsp = pkgs.writeShellScriptBin "nvim-lsp" ''
     exec ${neovim-lsp}/bin/nvim "$@"
   '';
   # Alias program for lsp
-  neovim-qt-lsp-alias = pkgs.writeShellScriptBin "nvim-qt-lsp" ''
+  neovim-qt-lsp = pkgs.writeShellScriptBin "nvim-qt-lsp" ''
     exec ${neovim-qt-lsp}/bin/nvim-qt "$@"
   '';
-in
-  {
-    inherit
-      neovim
-      neovim-qt
-      neovim-lsp neovim-lsp-alias
-      neovim-qt-lsp
-      neovim-qt-lsp-alias
-      ;
-  }
+}
