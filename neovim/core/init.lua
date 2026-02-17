d4dba53808853e83e7b22e16516be4c45fa8b469
xbreak <xbreak@users.noreply.github.com>
2026-02-17 20:47:54 +0100

    Restore `T` binding to `FSHere`

--- neovim/core/init.lua
+++ neovim/core/init.lua
@@ -804,7 +804,7 @@ do
     vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
     vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
     vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
-    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+    --vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
   end
 end
 
